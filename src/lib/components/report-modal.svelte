d722de46b9922ba6acd292e31ced33d98b87ab4f
Reinier Hernández <sasuke.reinier@gmail.com>
2023-02-01 09:23:39 -0300

    feat: added missing people form component

---
+++ src/lib/components/report-modal.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<div>
+    <form action="post">
+        <label>
+            Nombre del desaparecido
+            <input type="text" required>
+        </label>
+        <label>
+            Nombre del denunciante
+            <input type="text" required>
+        </label>
+        <div class="flex">
+            <label>
+                Email
+                <input type="email" name="email" id="email" required>
+            </label>
+            <label>
+                Teléfono
+                <input type="tel">
+            </label>
+        </div>
+        <label>
+            Mensaje
+            <textarea name="message" id="message" cols="30" rows="10" required></textarea>
+        </label>
+        <button type="submit">Enviar</button>
+    </form>
+</div>
+
+<style>
+    input, textarea {
+        border: 1px solid white;
+    }
+</style>
