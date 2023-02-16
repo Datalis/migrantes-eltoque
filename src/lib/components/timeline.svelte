db44a90e0f6b40e63ae98264bd9e4330059c1dd2
Reinier Hernández <sasuke.reinier@gmail.com>
2023-02-16 08:29:51 -0300

    perf: added months to timeline

--- src/lib/components/timeline.svelte
+++ src/lib/components/timeline.svelte
@@ -12,11 +12,37 @@
 		<button class="button">Muerte</button>
 		<button class="button">Desapariciones</button>
 	</div>
-	<div class="border border-light h-full rounded-xl mt-3" />
+	<div class="flex border border-light h-full rounded-xl mt-3 pt-5">
+        <div class="line year">2022</div>
+        <div class="line">Diciembre</div>
+        <div class="line">Noviembre</div>
+        <div class="line">Octubre</div>
+        <div class="line">Septiembre</div>
+        <div class="line">Agosto</div>
+        <div class="line">Julio</div>
+        <div class="line">Junio</div>
+        <div class="line">Mayo</div>
+        <div class="line">Abril</div>
+        <div class="line">Marzo</div>
+        <div class="line">Febrero</div>
+        <div class="line noline">Enero</div>
+    </div>
 </div>
 
 <style>
 	.button {
 		@apply rounded-xl text-light bg-dark px-2 py-1 border border-accent text-sm;
 	}
+
+    .line {
+        writing-mode: vertical-lr;
+        transform: rotate(180deg);
+        @apply text-gray opacity-50 text-sm w-1/12 border-l-2 border-light text-end h-full;
+    }
+    .year {
+        @apply text-accent border-l-4 opacity-100;
+    }
+    .noline {
+        @apply border-0;
+    }
 </style>
