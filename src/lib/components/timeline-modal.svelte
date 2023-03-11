80a1c8e42b946e9916fa31f3f1e3a84afc42c85b
Reinier Hernández <sasuke.reinier@gmail.com>
2023-03-11 11:00:54 -0300

    fix: event modal on mobile

--- src/lib/components/timeline-modal.svelte
+++ src/lib/components/timeline-modal.svelte
@@ -48,7 +48,6 @@
 			return;
 		}
 	};
-
 </script>
 
 <svelte:window on:keydown={handle_keydown} />
@@ -62,30 +61,20 @@
 	<button on:click={close} type="button" class="close absolute right-4 top-4">
 		<X width="32" height="32" />
 	</button>
-	<div class="h-full w-full bg-accent text-light">
-		<div class="event-info w-screen h-screen shrink-0 event-{event.id}">
-			<img
-				class="p-2 absolute z-0"
-				src={getImageFromEvent(event)}
-				alt={event.name}
-				loading="lazy"
-			/>
-			<div class="px-8 mt-40 md:mt-24 relative z-10">
-				<h3 class="uppercase font-bold mb-2">
-					{months[event.date.getMonth()]}
-					{event.date.getFullYear()}
-				</h3>
-				<p class="">{event.description}</p>
-				<p class="mt-2">
-					<span class="font-bold uppercase">Fuente:</span>
-					<a
-						class="underline underline-offset-2"
-						href={event.links}
-						target="_blank"
-						rel="noreferrer">{event.source}</a
-					>
-				</p>
-			</div>
+	<div class="event-info bg-accent text-light w-screen h-screen shrink-0 event-{event.id}">
+		<img class="p-2 absolute z-0" src={getImageFromEvent(event)} alt={event.name} loading="lazy" />
+		<div class="px-8 mt-40 md:mt-24 relative z-10">
+			<h3 class="uppercase font-bold mb-2">
+				{months[event.date.getMonth()]}
+				{event.date.getFullYear()}
+			</h3>
+			<p class="">{event.description}</p>
+			<p class="mt-2">
+				<span class="font-bold uppercase">Fuente:</span>
+				<a class="underline underline-offset-2" href={event.links} target="_blank" rel="noreferrer"
+					>{event.source}</a
+				>
+			</p>
 		</div>
 	</div>
 </div>
