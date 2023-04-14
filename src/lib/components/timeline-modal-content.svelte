81e5198829269294fbce850ce17f4be2666fbae8
Reinier Hernández <sasuke.reinier@gmail.com>
2023-04-14 16:23:48 -0300

    perf: event modal in mobile

---
+++ src/lib/components/timeline-modal-content.svelte
@@ -0,0 +1,16 @@
+<script>
+	import ArrowDown from '$lib/assets/images/arrow-down.svg?component';
+</script>
+
+<div class="md:w-96 text-center flex flex-col items-center">
+	<h2 class="font-sans font-extrabold text-3xl text-light mb-7">
+		Tenemos más historias que contarte
+	</h2>
+	Cada círculo corresponde a un evento migratorio. Puedes seguir explorando cada uno de ellos en esta
+	herramienta interactiva y seleccionar las categorías que quieras ver.
+	<div
+		class="w-14 h-14 rounded-full border-accent border-2 p-2 flex items-center mt-4 cursor-pointer"
+	>
+		<ArrowDown />
+	</div>
+</div>
